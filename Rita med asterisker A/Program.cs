822c2e02faad68d41b2cfce2662bfc3e55fd80fd
hobbe84 <hobbe84@bredband.net>
2015-01-08 15:59:12 +0100

    Labb 2 A

---
+++ Rita med asterisker A/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rita_med_asterisker_A
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Loopar varje rad 25 gånger
+            for (int rad = 0; rad < 25; rad++)
+            {
+                if (rad % 2 == 1) // Lägger mellanrummet på varannan rad, jämnt radnummer
+                {
+                    Console.Write(" ");
+                }
+                switch (rad % 3) // kör igenom switchsatsen cases tills påståendet blir sant, startar sen om på case 0 
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                }
+                // 
+                for (int col = 0; col < 39; col++) // Ritar 39 stjänor på varje rad
+                {
+                    Console.Write("* ");
+
+                }
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.ReadKey(); // Låter användaren avsluta programmet
+            }
+        }
+    }
+}
